8b3a442eb493f618395e37200839ddde9862474e
April <ghwon4640@gmail.com>
2025-08-13 03:20:03 +0900

    controller

--- FastAPI_study/main.py
+++ FastAPI_study/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from typing import Union
+from controller import items, users
 
 app = FastAPI()
+
+app.include_router(items.router)
+app.include_router(users.router)
 
 @app.get('/')
 def read_root():
     return {"hello": "world"}  # 따옴표 일관성
-
-@app.get('/items/{item_id}')  # 슬래시 추가
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {'item_id': item_id, 'q': q}
