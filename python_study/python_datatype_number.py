4e8dae15c189b3016fe4010f0c0a24d0db3a66f6
April <ghwon4640@gmail.com>
2025-09-08 14:22:20 +0900

    number

---
+++ python_study/python_datatype_number.py
@@ -0,0 +1,16 @@
+# 숫자형 (Number)
+# 정수 (int)
+
+print('-----int-----\n')
+print(type(222))
+print()
+age = 35
+temperature = -10
+print(f'age의 값 : {age}\nage의 자료형 : {type(age)}')
+print(f'temperature의 값 : {temperature}\ntemperature의 자료형 : {type(temperature)}')
+print('\n-----float-----\n')
+# 실수 (float)
+height = 175.5
+pi = 3.14159
+print(f'height의 값 : {height}\nheight의 자료형 : {type(height)}')
+print(f'pi의 값 : {pi}\npi의 자료형 : {type(pi)}')
